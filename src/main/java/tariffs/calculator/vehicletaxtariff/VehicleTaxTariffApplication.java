9142222059a69b1e4a71eb1cfef33415af9fbbd4
Alan Ramos <alan.ramos@inera.se>
2022-10-16 14:51:09 +0200

    Initial commit with project structure and SpringBoot support

---
+++ src/main/java/tariffs/calculator/vehicletaxtariff/VehicleTaxTariffApplication.java
@@ -0,0 +1,13 @@
+package tariffs.calculator.vehicletaxtariff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTaxTariffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleTaxTariffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tariffs/calculator/vehicletaxtariff/VehicleTaxTariffApplicationTests.java
@@ -0,0 +1,13 @@
+package tariffs.calculator.vehicletaxtariff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTaxTariffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
