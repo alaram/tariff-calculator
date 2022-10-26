bc056a57dcb9dd5fdcdbd5dad33a36caa33c2221
Alan Ramos <alan.ramos@inera.se>
2022-10-26 19:33:05 +0200

    Added database sql and test for SpringBoot to test the repository

--- src/test/java/tariffs/calculator/vehicletaxtariff/VehicleTaxTariffApplicationTests.java
+++ src/test/java/tariffs/calculator/vehicletaxtariff/VehicleTaxTariffApplicationTests.java
@@ -1,12 +1,27 @@
 package tariffs.calculator.vehicletaxtariff;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tariffs.calculator.vehicletaxtariff.repositories.TariffRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class VehicleTaxTariffApplicationTests {
 
+    @Autowired
+    TariffRepository tariffRepository;
+
     @Test
-    void contextLoads() {
+    void testTariffRepository() {
+        long count = tariffRepository.count();
+
+        assertThat(count).isGreaterThan(0);
     }
+
+    @Test
+    void contextLoads() {}
 }
---
+++ src/test/java/tariffs/calculator/vehicletaxtariff/repositories/SpringBootJpaTestSlice.java
@@ -0,0 +1,53 @@
+package tariffs.calculator.vehicletaxtariff.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import org.springframework.test.annotation.Commit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import tariffs.calculator.vehicletaxtariff.domain.Tariff;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+public class SpringBootJpaTestSlice {
+
+    @Autowired
+    TariffRepository tariffRepository;
+
+    /**
+     * The purpose of this is to ensure/demonstrate
+     * a) That test will run in the specified order
+     * b) That test will perform the transaction so that the transaction
+     *    does not rollback, thus the datasource is empty for the coming test.
+     *    99% of the cases, we want this to be but testing transaction is for
+     *    demonstrating purposes.
+     */
+    @Commit
+    @Order(1)
+    @Test
+    void testJpaTestSlice() {
+        long countBefore = tariffRepository.count();
+        assertThat(countBefore).isEqualTo(0);
+
+        tariffRepository.save(new Tariff("05:00-05:59", "Stockholm", "1"));
+
+        long countAfter = tariffRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSliceTransaction() {
+        long countBefore = tariffRepository.count();
+
+        assertThat(countBefore).isEqualTo(1);
+    }
+
+}
