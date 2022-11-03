573c00b425526fbdfe5c701e742269af07426bc7
Alan Ramos <alan.ramos@inera.se>
2022-11-03 21:06:13 +0100

    Updated Swagger Config

--- src/main/java/tariffs/calculator/vehicletaxtariff/config/SwaggerConfig.java
+++ src/main/java/tariffs/calculator/vehicletaxtariff/config/SwaggerConfig.java
@@ -10,12 +10,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
@@ -25,7 +23,16 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Configuration
 public class SwaggerConfig {
 
-    private static final Contact DEVELOPER_CONTACT = new Contact("Alan Ramos", "http://antigo.se", "alan.ramos@antigo.se");
+    private static final String API_TITLE = "Vehicle Tariff Calculator API";
+    private static final String API_DESC = "Rest services for Vehicle Tariff Calculator API";
+    private static final String API_LIC = "";
+    private static final String API_LIC_URL = "http://antigo.se";
+    private static final String API_LIC_VERSION = "1.0";
+    private static final String DEVELOPER_CONTACT_NAME = "Alan Ramos";
+    private static final String DEVELOPER_CONTACT_EMAIL = "alan.ramos@antigo.se";
+    private static final String DEVELOPER_CONTACT_URL = "http://antigo.se";
+    private static final String APP_BASE_PACKAGE = "tariffs.calculator.vehicletaxtariff.web";
+    private static final Contact DEVELOPER_CONTACT = new Contact(DEVELOPER_CONTACT_NAME, DEVELOPER_CONTACT_URL, DEVELOPER_CONTACT_EMAIL);
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList(APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE));
 
     /**
@@ -34,18 +41,14 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket api() {
-        //List<SecurityScheme> schemeList = new ArrayList<>();
-        //schemeList.add(new BasicAuth("basicAuth"));
-
         return new Docket(DocumentationType.SWAGGER_2)
                    .select()
-                   .apis(RequestHandlerSelectors.basePackage("tariffs.calculator.vehicletaxtariff.web"))
+                   .apis(RequestHandlerSelectors.basePackage(APP_BASE_PACKAGE))
                    .paths(PathSelectors.any())
                    .build()
                    .produces(DEFAULT_PRODUCES_AND_CONSUMES)
                    .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
                    .apiInfo(apiInfo());
-                   //.securitySchemes(schemeList);
     }
 
     /**
@@ -54,12 +57,12 @@ public class SwaggerConfig {
      */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                   .title("Vehicle Tariff Calculator API")
-                   .description("Rest services for Vehicle Tariff Calculator API")
+                   .title(API_TITLE)
+                   .description(API_DESC)
                    .contact(DEVELOPER_CONTACT)
-                   .license("")
-                   .licenseUrl("http://antigo.se")
-                   .version("1.0")
+                   .license(API_LIC)
+                   .licenseUrl(API_LIC_URL)
+                   .version(API_LIC_VERSION)
                    .build();
     }
 }
